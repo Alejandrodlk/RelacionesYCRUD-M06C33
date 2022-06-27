dda6778c5faecb71e02c939136f7b36822a6398f
Alejandrodlk <ale_165851@hotmail.com>
2022-06-27 19:25:06 -0300

    crud y relaciones actor genero y movie

---
+++ src/controllers/actorsController.js
@@ -0,0 +1,35 @@
+const db = require('../database/models');
+const sequelize = db.sequelize;
+
+
+const actorsController = {
+    'list': (req, res) => {
+        db.Actor.findAll({
+
+            include : [
+                {
+                    association : 'movies',
+                    include : [
+                        {
+                            association : 'genre',
+                            attributes : ['name'] // para mostrar solo la columna "name" correspondiente a genero
+                        }
+                    ]
+                }
+            ]
+
+           /* include: [   
+                {
+                    association : 'movies',   // movies se relaciona con actor pero genre NO, POR ESO SE HACE DE ESTA FORMA
+                    include : ['genre']
+                }
+            ] */
+        })
+            .then(actors => {
+                return res.send(actors)
+            })
+        }
+
+}
+
+module.exports = actorsController;
